25e22b6f68ddece3aa8a20463067bc5f00a3434d
brock cameron <114433184+bthecreat0r@users.noreply.github.com>
2024-03-22 09:30:20 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-window.addEventListener('load', calculateTime)
+window.addEventListener('load', calculateTime);
+
 function calculateTime(){
-    var date=new Date();
-    var dayNumber=date.getDay();
-    var hour= date.getHours();
+    var date = new Date();
+    var dayNumber = date.getDay();
+    var hour = date.getHours();
     var minute = date.getMinutes();
     var ampm = hour >= 12 ? 'PM' : 'AM';
     var dayNames = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
     hour = hour % 12;
     hour = hour == 12 ? '12' : hour;
-    hour = hour < 10 ? '0' + minute : minute;
+    hour = hour < 10 ? '0' + hour : hour;
     minute = minute < 10 ? '0' + minute : minute;
 
-    document.getElementByld("day").innerHTML = dayNames[dayNumber];
-    document.getElementByld("hour").innerHTML = hour;
-    document.getElementByld("minute").innerHTML = minute;
-    document.getElementByld("ampm").innerHTML = ampm;
+    document.getElementById("day").innerHTML = dayNames[dayNumber];
+    document.getElementById("hour").innerHTML = hour;
+    document.getElementById("minute").innerHTML = minute;
+    document.getElementById("ampm").innerHTML = ampm;
 
     setTimeout(calculateTime, 200);
 }
